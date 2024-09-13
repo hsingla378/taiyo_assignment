237e755c5c9fbcdc6417c0af36916d0df3d3b219
Himanshu Singla <himanshusingla@Himanshus-MacBook-Pro.local>
2024-09-13 20:22:30 +0530

    create contacts store

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,11 +1,49 @@
 import React from "react";
 import Header from "../components/Header";
+import { useDispatch, useSelector } from "react-redux";
 
 const Contacts = () => {
+  const [activeContact, setActiveContact] = React.useState(null);
+  const contacts = useSelector((store) => store?.contact?.contacts);
+  const dipatch = useDispatch();
+
+  console.log("contacts", contacts);
+
   return (
     <div className="text-black">
       <Header />
-      Contacts
+      <h1 className="text-2xl text-bold mb-8">Contacts</h1>
+      <button className="bg-green-800 rounded-lg text-white p-2 px-4">
+        Create Contact
+      </button>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 text-center">
+        {contacts?.map((contact, index) => (
+          <div
+            key={contact?.id}
+            className="relative bg-blue-300 p-4 rounded-lg text-xl flex flex-col gap-4"
+          >
+            <div>
+              <span className="font-bold">First Name:</span>{" "}
+              {contact?.firstName}
+            </div>
+            <div>
+              <span className="font-bold">Last Name:</span> {contact?.lastName}
+            </div>
+            <div>
+              <span className="font-bold">isActive</span>{" "}
+              {contact?.active ? "True" : "False"}
+            </div>
+            <div className="flex justify-between w-full mt-4 gap-2">
+              <button className="bg-blue-900 text-white p-2 w-full rounded-lg">
+                Edit
+              </button>
+              <button className="bg-red-700 text-white p-2 w-full rounded-lg">
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/utils/contactSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const contactSlice = createSlice({
+  name: "contact",
+  initialState: {
+    contacts: [
+      {
+        id: 1,
+        firstName: "John",
+        lastName: "Smith",
+        active: true,
+      },
+      {
+        id: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+        active: true,
+      },
+      {
+        id: 3,
+        firstName: "James",
+        lastName: "Bond",
+        active: false,
+      },
+      {
+        id: 4,
+        firstName: "Bruce",
+        lastName: "Wayne",
+        active: true,
+      },
+      {
+        id: 5,
+        firstName: "Clark",
+        lastName: "Kent",
+        active: true,
+      },
+      {
+        id: 6,
+        firstName: "Diana",
+        lastName: "Prince",
+        active: false,
+      },
+    ],
+  },
+  reducers: {
+    addContact: (state, action) => {
+      state?.contacts?.push(action.payload);
+    },
+    removeContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        (contact) => contact?.id !== action.payload
+      );
+    },
+    clearContacts: (state) => {
+      state.contacts = [];
+    },
+  },
+});
+
+export const { addContact, removeContact, clearContacts } =
+  contactSlice.actions;
+
+export default contactSlice.reducer;
